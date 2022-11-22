2442259389c44b924e82e3e12ef3c3329253b791
Xavi Carter <116882813+XaviCarter@users.noreply.github.com>
2022-11-22 10:59:10 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,53 @@
+var mySong, amplitude, play
+
+function preload() {
+    mySong = loadSound('assets/massiveAttackAngel.mp3');
+}
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
+    rectMode(CENTER);
+    
+
+    amplitude = new p5.Amplitude()
+    
+    background(0);
+    noStroke()
+
+    play = false;
+    var button = createButton(" Play ")
+    button.center()
+    button.addClass('btn')
+
+    button.mousePressed(function() {
+        play = true;
+        mySong.play()
+        button.hide()
+    })
+
 }
 
 function draw() {
-    background(220);
+    if(play == true){
+        var level = amplitude.getLevel()
+        var vol = map(level, 0, 1, 0, 1000)
+        var col = map(level, 0, 1, 0, 255)
+    
+        background(col)
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+        console.log(amplitude)
+    
+ 
+
+        fill(col, 255, 255);
+        ellipse(width/2, height/2, vol);
+
+        fill(255, 255, col);
+        ellipse(width/2, height/2, vol/2);
+
+        fill(255, col, 255);
+        ellipse(width/2, height/2, vol/4);  
+    }
+
 }
